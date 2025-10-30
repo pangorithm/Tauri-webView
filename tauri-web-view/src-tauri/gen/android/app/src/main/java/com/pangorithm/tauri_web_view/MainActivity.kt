a7a4b590a51012ba0c82a64607ffa150ff04bad9
pangorithm <phs559800@gmail.com>
2025-10-30 23:06:53 +0900

    android init

---
+++ tauri-web-view/src-tauri/gen/android/app/src/main/java/com/pangorithm/tauri_web_view/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pangorithm.tauri_web_view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
